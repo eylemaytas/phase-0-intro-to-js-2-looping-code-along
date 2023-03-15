cf18a6c85dcff1d80e8c5571c56ce1d606e6d9f0
Eylem Aytas <eylem.aytas@hotmail.com>
2023-03-15 17:50:53 -0400

    Adds package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function countDown(num) { 
+while (num>=0) {
+    console.log(num--)
+}
+    
+}
+countDown(10)
+
+
+
+
+
+
+
+const names= ["Charlie", "Samip", "Ali"];
+function writeCards(names, birthday) {
+    const messages= [];
+for (let i=0; i <names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+}
+return messages;
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"],"birthday" ));
