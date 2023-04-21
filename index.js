8a54e2eae46ada751642080131f35e7048d23686
Eylem Aytas <eylem.aytas@hotmail.com>
2023-04-21 13:14:44 -0400

    Modifies index.js

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+
+const names = ["Charlie", "Samip", "Ali"]
+function writeCards(names, birthday) {
+const messages = []
+for(let i=0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+}
+return messages
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"]))
+
+
+
+
 function countDown(num) { 
+    while (num>=0) {
+        console.log(num--)
+    }
+        
+    }
+    countDown(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function countDown(num) { 
 while (num>=0) {
     console.log(num--)
 }
@@ -21,3 +57,4 @@ for (let i=0; i <names.length; i++) {
 return messages;
 }
 console.log(writeCards(["Charlie", "Samip", "Ali"],"birthday" ));
+*/
